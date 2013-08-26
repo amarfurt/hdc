df0fdac83c894044cc662a67e56d511ffd0ca466
amarfurt <amarfurt@student.ethz.ch>
2013-08-26 17:37:28 +0200

    Moved forms to own package within controllers package.

--- app/controllers/forms/Login.java
+++ app/controllers/forms/Login.java
@@ -1,4 +1,4 @@
-package controllers;
+package controllers.forms;
 
 import models.User;
 
---
+++ app/controllers/forms/SpaceForm.java
@@ -0,0 +1,15 @@
+package controllers.forms;
+
+public class SpaceForm {
+
+	public String name;
+	public String visualization;
+
+	public String validate() {
+		// TODO check whether space with same name exists already
+		// check whether given visualization exists and is installed by user(?)
+		// not necessary if dropdown list
+		return null;
+	}
+
+}
