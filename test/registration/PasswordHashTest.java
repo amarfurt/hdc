ceb83cfa641ca70b0da9d20e7236a621c3279c0e
amarfurt <amarfurt@student.ethz.ch>
2013-09-05 16:10:01 +0200

    Adding password hashing functionality...

---
+++ test/registration/PasswordHashTest.java
@@ -0,0 +1,44 @@
+package registration;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import org.junit.Test;
+
+import utils.PasswordHash;
+
+public class PasswordHashTest {
+
+	@Test
+	public void sameHash() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		for (int i = 0; i < 100; i++) {
+			String password = "" + i;
+			String hash = PasswordHash.createHash(password);
+			String secondHash = PasswordHash.createHash(password);
+			assertFalse(hash.equals(secondHash));
+		}
+	}
+
+	@Test
+	public void wrongPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		for (int i = 0; i < 100; i++) {
+			String password = "" + i;
+			String hash = PasswordHash.createHash(password);
+			String wrongPassword = "" + (i + 1);
+			assertFalse(PasswordHash.validatePassword(wrongPassword, hash));
+		}
+	}
+
+	@Test
+	public void correctPassword() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		for (int i = 0; i < 100; i++) {
+			String password = "" + i;
+			String hash = PasswordHash.createHash(password);
+			assertTrue(PasswordHash.validatePassword(password, hash));
+		}
+	}
+
+}
