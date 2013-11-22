996f5688bed7765142be68f4b1764afbc7781729
amarfurt <amarfurt@student.ethz.ch>
2013-11-22 11:26:12 +0100

    Also changed the appName.

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "healthbank"
+  val appName         = "hdc"
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
