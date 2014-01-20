87d8e7b9511e7de225e97c3df9b8ace7f8028804
Andreas Marfurt <amarfurt@inf.ethz.ch>
2014-01-20 16:43:09 +0100

    DatabaseConversion fixed: Have to convert database types to model types when deserializing.

--- platform/app/utils/db/DatabaseConversion.java
+++ platform/app/utils/db/DatabaseConversion.java
@@ -1,11 +1,18 @@
 package utils.db;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import models.Model;
 
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
@@ -31,7 +38,8 @@ public class DatabaseConversion {
 	/**
 	 * Converts a database object back into a model.
 	 */
-	public static <T extends Model> T toModel(Class<T> modelClass, DBObject dbObject) throws DatabaseConversionException {
+	public static <T extends Model> T toModel(Class<T> modelClass, DBObject dbObject)
+			throws DatabaseConversionException {
 		T modelObject;
 		try {
 			modelObject = modelClass.newInstance();
@@ -43,7 +51,7 @@ public class DatabaseConversion {
 		for (Field field : modelClass.getFields()) {
 			if (dbObject.keySet().contains(field.getName())) {
 				try {
-					field.set(modelObject, dbObject.get(field.getName()));
+					field.set(modelObject, convert(field.getGenericType(), dbObject.get(field.getName())));
 				} catch (IllegalArgumentException e) {
 					throw new DatabaseConversionException(e);
 				} catch (IllegalAccessException e) {
@@ -65,4 +73,73 @@ public class DatabaseConversion {
 		}
 		return models;
 	}
+
+	/**
+	 * Converts an object retrieved from the database to the corresponding type.
+	 */
+	private static Object convert(Type type, Object value) {
+		if (type instanceof ParameterizedType) {
+			ParameterizedType parameterizedType = (ParameterizedType) type;
+			if (parameterizedType.getRawType().equals(Map.class)) {
+				return convertToMap(type, value);
+			} else if (parameterizedType.getRawType().equals(Set.class)) {
+				return convertToSet(type, value);
+			} else if (parameterizedType.getRawType().equals(List.class)) {
+				return convertToList(type, value);
+			}
+		}
+		return value;
+	}
+
+	/**
+	 * Converts a BasicDBObject into a map (keys are always strings because of JSON serialization).
+	 */
+	private static Map<String, Object> convertToMap(Type type, Object value) {
+		BasicDBObject dbObject = (BasicDBObject) value;
+		if (type instanceof ParameterizedType) {
+			Type valueType = ((ParameterizedType) type).getActualTypeArguments()[1];
+			Map<String, Object> map = new HashMap<String, Object>();
+			for (String key : dbObject.keySet()) {
+				map.put(key, convert(valueType, dbObject.get(key)));
+			}
+			return map;
+		} else {
+			return new HashMap<String, Object>(dbObject);
+		}
+	}
+
+	/**
+	 * Converts a BasicDBList into a set.
+	 */
+	private static Set<Object> convertToSet(Type type, Object value) {
+		BasicDBList dbList = (BasicDBList) value;
+		if (type instanceof ParameterizedType) {
+			Type valueType = ((ParameterizedType) type).getActualTypeArguments()[0];
+			Set<Object> set = new HashSet<Object>();
+			for (Object element : dbList) {
+				set.add(convert(valueType, element));
+			}
+			return set;
+		} else {
+			return new HashSet<Object>(dbList);
+		}
+	}
+
+	/**
+	 * Converts a BasicDBList into a list.
+	 */
+	private static List<Object> convertToList(Type type, Object value) {
+		BasicDBList dbList = (BasicDBList) value;
+		if (type instanceof ParameterizedType) {
+			Type valueType = ((ParameterizedType) type).getActualTypeArguments()[0];
+			List<Object> list = new ArrayList<Object>();
+			for (Object element : dbList) {
+				list.add(convert(valueType, element));
+			}
+			return list;
+		} else {
+			return new ArrayList<Object>(dbList);
+		}
+	}
+
 }
