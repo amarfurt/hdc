59ef43b992575b1ccaf90da5e1a55386e1dcf952
Andreas Marfurt <amarfurt@inf.ethz.ch>
2014-10-02 11:32:42 +0200

    Updated versions, fixed bower and karma includes.

--- visualizations/clock/karma.conf.js
+++ visualizations/clock/karma.conf.js
@@ -12,10 +12,11 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'app/bower_components/angular/angular.js',
+      'app/bower_components/angular-bootstrap/ui-bootstrap.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-route/angular-route.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
-      'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
 
